55821c5a20ff3507d14a3fe8f123ab32c88b0b31
Jane Chen <jchen010197@gmail.com>
2014-03-16 22:01:10 -0400

    added hw 7b

--- 07-Quicksort/Quicksort2.java
+++ 07-Quicksort/Quicksort2.java
@@ -3,15 +3,21 @@ import java.util.*;
 
 public class Quicksort2 {
 
+    // quicksort that groups duplicate pivots together
+    // faster than original quicksort routine
+
     public int partition(int[] a, int left, int right) {
-	int pivot = left + Math.Random() * (right - low);
-	// swap pivot with right value
+	int pivot = (int)(Math.random() * (right - left) + left);
+	// switch pivot with the farthest right value
+	// to get the pivot out of the way
 	int temp=a[pivot];
 	a[pivot] = a[right];
 	a[right] = temp;
+
 	int wall=left;
-	for (int i=left; i<right-1; i++) {
-	    if (a[i] > a[right]) { // note: a[right] now holds the pivot value
+	for (int i=left; i<right; i++) {
+	    if (a[i] < a[right]) {
+	    //if (a[i] == a[pivot]) {
 		temp = a[wall];
 		a[wall] = a[i];
 		a[i] = temp;
@@ -24,10 +30,23 @@ public class Quicksort2 {
 	return wall;
     }
 
-    public int[] quicksort(int[] a) {
-	if (a.length <= 1) {
-	    return a;
+    public void quicksort(int[] a, int low, int high) {
+	if (low < high) {
+	    int pivot = partition(a,low,high);
+	    quicksort(a,low,pivot-1);
+	    quicksort(a,pivot+1,high);
 	}
-    }
+    }  
 
+    public static void main(String[] args) {
+	Random r = new Random();
+	int[] a = new int[50];
+	for (int i=0; i<a.length; i++) {
+	    a[i] = r.nextInt(100);
+	}
+	Quicksort2 qs = new Quicksort2();
+	System.out.println(Arrays.toString(a));
+	qs.quicksort(a,0,a.length-1);
+	System.out.println(Arrays.toString(a));
+    }
 }
