2a2200fd645498fe0ec2d452da9a86c1e273051c
Jane Chen <jchen010197@gmail.com>
2014-05-23 06:05:29 -0400

    preparation

---
+++ FridayTest/Test.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String input = sc.nextLine();
+	System.out.println(input);
+
+	// note: use input.equals(blahblah)
+
+    }
+
+}
